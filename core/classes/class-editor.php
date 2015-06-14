db640bfe0cb529072900b57ad05a5748fe5ea281
Brajesh Singh <brajesh@cosmiccoders.com>
2015-06-14 10:24:35 +0530

    Break into multiple files

---
+++ core/classes/class-editor.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * Main controller class
+ * stores various forms and delegates the post saving to appropriate form
+ * 
+ */
+
+class BPSimpleBlogPostEditor {
+
+    private static $instance;
+    var $forms = array(); // array of Post Forms(multiple post forms)
+    private $self_url;
+
+    private function __construct() {
+        $this->self_url = plugin_dir_url( __FILE__ );
+
+        //hook save action to init
+        add_action( 'bp_ready', array( $this, 'save' ) );
+    }
+
+    /**
+     * Factory method for singleton object
+     * 
+     * @return BPSimpleBlogPostEditor
+     */
+    public static function get_instance() {
+        if ( !isset( self::$instance ) )
+            self::$instance = new self();
+        
+        return self::$instance;
+    }
+
+    /**
+     * Register a form
+     * 
+     * @param BPSimpleBlogPostEditForm $form 
+     */
+    public function register_form( $form ) {
+        $this->forms[$form->id] = $form; //save/overwrite
+    }
+
+    /**
+     *
+     * @param string $form_name
+     * @return BPSimpleBlogPostEditForm|boolean 
+     */
+    public function get_form_by_name( $form_name ) {
+        $id = md5(trim( $form_name ) );
+        return $this->get_form_by_id( $id );
+    }
+
+    /**
+     * Returns the Form Object
+     * 
+     * @param string $form_id
+     * @return BPSimpleBlogPostEditForm|boolean 
+     */
+    public function get_form_by_id( $form_id ) {
+
+        if ( isset( $this->forms[$form_id] ) )
+            return $this->forms[$form_id];
+        return false;
+    }
+
+    /**
+     * Save a post
+     * 
+     * Delegates the task to  BPSimpleBlogPostEditForm::save() of appropriate form(which was submitted)
+     * 
+     * @return type 
+     */
+    public function save() {
+        if ( !empty($_POST['bp_simple_post_form_subimitted'] ) ) {
+            //yeh form was submitted
+            //get form id
+            $form_id = $_POST['bp_simple_post_form_id'];
+            $form = $this->get_form_by_id( $form_id );
+
+            if ( !$form )
+                return; //we don't need to do anything
+                
+//so if it is a registerd form, let the form handle it
+
+            $form->save(); //save the post and redirect properly
+        }
+    }
+
+}
---
+++ core/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+
+//API for general use
+/**
+ * Create and Register a New Form Instance, Please make sure to call it before bp_init action to make the form available to the controller logic
+ * @param type $form_name:string, a unique name, It can contain letters or what ever eg. my_form or my form or My Form 123
+ * @param type $settings:array,It governs what is shown in the form and how the form will be handled, possible values are
+ *  array('post_type'=>'post'|'page'|valid_post_type,'post_status'=>'draft'|'publish'|'valid_post_status','show_categories'=>true|false,'current_user_can_post'=>true|false
+ * @return BPSimpleBlogPostEditForm 
+ */
+function bp_new_simple_blog_post_form($form_name, $settings) {
+
+    $form = new BPSimpleBlogPostEditForm($form_name, $settings);
+    $editor = BPSimpleBlogPostEditor::get_instance();
+    $editor->register_form($form);
+
+    return $form;
+}
+
+//get a referenace to a particular form instance
+function bp_get_simple_blog_post_form($name) {
+    $editor = BPSimpleBlogPostEditor::get_instance();
+    return $editor->get_form_by_name($name);
+}
+
+//get a referenace to a particular form instance
+function bp_get_simple_blog_post_form_by_id($form_id) {
+    $editor = BPSimpleBlogPostEditor::get_instance();
+    return $editor->get_form_by_id($form_id);
+}
