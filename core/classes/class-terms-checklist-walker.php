d4e0f80fcf9b8c7c8448ad2bbed2f0e0ac6bd4ef
Brajesh Singh <brajesh@cosmiccoders.com>
2015-06-14 10:29:57 +0530

    Update the checklist terms walker

--- core/classes/class-terms-checklist-walker.php
+++ core/classes/class-terms-checklist-walker.php
@@ -1,39 +1,41 @@
 <?php
 
-
 /**
  * A Taxonomy Walker class to fix the input name of the taxonomy terms
  * 
  */
 class BPSimplePostTermsChecklistWalker extends Walker {
 
-    var $tree_type = 'category';
-    var $db_fields = array('parent' => 'parent', 'id' => 'term_id'); //TODO: decouple this
+	public $tree_type = 'category';
+	public $db_fields = array( 'parent' => 'parent', 'id' => 'term_id' ); //TODO: decouple this
 
-    function start_lvl(&$output, $depth = 0, $args = array()) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "$indent<ul class='children'>\n";
-    }
+	public function start_lvl ( &$output, $depth = 0, $args = array() ) {
+		
+		$indent = str_repeat( "\t", $depth );
+		$output .= "$indent<ul class='children'>\n";
+	}
 
-    function end_lvl(&$output, $depth = 0, $args = array()) {
-        $indent = str_repeat("\t", $depth);
-        $output .= "$indent</ul>\n";
-    }
+	public function end_lvl ( &$output, $depth = 0, $args = array() ) {
+		$indent = str_repeat( "\t", $depth );
+		$output .= "$indent</ul>\n";
+	}
 
-    function start_el(&$output, $category, $depth = 0, $args = array(), $id = 0) {
-        extract($args);
-        if (empty($taxonomy))
-            $taxonomy = 'category';
+	public function start_el ( &$output, $category, $depth = 0, $args = array(), $id = 0 ) {
+		
+		extract( $args );
+		
+		if ( empty( $taxonomy ) )
+			$taxonomy = 'category';
 
 
-        $name = 'tax_input[' . $taxonomy . ']';
+		$name = 'tax_input[' . $taxonomy . ']';
 
-        $class = in_array($category->term_id, $popular_cats) ? ' class="popular-category"' : '';
-        $output .= "\n<li id='{$taxonomy}-{$category->term_id}'$class>" . '<label class="selectit"><input value="' . $category->term_id . '" type="checkbox" name="' . $name . '[]" id="in-' . $taxonomy . '-' . $category->term_id . '"' . checked(in_array($category->term_id, $selected_cats, false), true, false) . disabled(empty($args['disabled']), false, false) . ' /> ' . esc_html(apply_filters('the_category', $category->name)) . '</label>';
-    }
+		$class = in_array( $category->term_id, $popular_cats ) ? ' class="popular-category"' : '';
+		$output .= "\n<li id='{$taxonomy}-{$category->term_id}'$class>" . '<label class="selectit"><input value="' . $category->term_id . '" type="checkbox" name="' . $name . '[]" id="in-' . $taxonomy . '-' . $category->term_id . '"' . checked( in_array( $category->term_id, $selected_cats, false ), true, false ) . disabled( empty( $args['disabled'] ), false, false ) . ' /> ' . esc_html( apply_filters( 'the_category', $category->name ) ) . '</label>';
+	}
 
-    function end_el(&$output, $category, $depth = 0, $args = array()) {
-        $output .= "</li>\n";
-    }
+	public function end_el ( &$output, $category, $depth = 0, $args = array() ) {
+		$output .= "</li>\n";
+	}
 
 }
